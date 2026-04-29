e911329e50b3e6c6495862e491f9efaf26e099a7
John Carl Ramirez <135854006+cjohnramirez@users.noreply.github.com>
2026-04-29 16:03:51 +0800

    hotfix: another attempt to remove nested repo in frontend (#5)

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import SignupPage from "./login/page";
+
+export default function Home() {
+  return (
+    <SignupPage />
+  );
+}
