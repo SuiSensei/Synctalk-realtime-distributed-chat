976219d85ba149817bdd1f0c5d1dd8ff9916b284
Francis Adrian Esteban <estebanfrancisadrian@gmail.com>
2026-05-03 22:08:18 +0800

    feat: added chat options dropdown for chat bar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import { NavigationBar } from "@/components/navigation-bar/navigation-bar";
+import ChatBar from "@/components/chat-bar/chat-options-dropdown";
 
 export default function Home() {
   return (
 
     <div className="min-h-screen bg-[#141414] p-4">
-      <NavigationBar />
+      <ChatBar/>
     </div>
   );
 }
